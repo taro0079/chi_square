8bce4e5171514e588c636def7b6e29d859211626
Taro Morita <morita0079@gmail.com>
2022-09-29 00:31:32 +0900

    write some test

--- main_test.go
+++ main_test.go
@@ -2,6 +2,125 @@ package main
 
 import "testing"
 
-func TestJudgement(t *testing.T){
-				tests:=
+func TestJudgement(t *testing.T) {
+	tests := []struct {
+		name string
+		arg  float64
+		want bool
+	}{
+		{
+			name: "when chi2 is under the threshold",
+			arg:  1,
+			want: false,
+		},
+		{
+			name: "when chi2 is over the threshold",
+			arg:  4,
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := judgement(tt.arg); got != tt.want {
+				t.Errorf("judgement() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestOriginalNonConversion(t *testing.T) {
+	type args struct {
+		originalParameter  float64
+		originalConversion float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		{
+			name: "when originalParameter is 1000 and originalConversion is 100",
+			args: args{originalParameter: 1000.0, originalConversion: 100.0},
+			want: 900.0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := originalNonConversion(tt.args.originalParameter, tt.args.originalConversion); got != tt.want {
+				t.Errorf("originalNonConversion() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestTestNonConversion(t *testing.T) {
+	type args struct {
+		testParameter  float64
+		testConversion float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		{
+			name: "when testParameter is 1000 and testConversion is 100",
+			args: args{testParameter: 1000.0, testConversion: 100.0},
+			want: 900.0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := testNonConversion(tt.args.testParameter, tt.args.testConversion); got != tt.want {
+				t.Errorf("testNonConversion() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+
+}
+
+func TestSumVar(t *testing.T) {
+	tests := []struct {
+		name string
+		arg  [][]float64
+		want float64
+	}{
+		{
+			name: "sum all",
+			arg:  [][]float64{{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}},
+			want: 21.0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sumvar(tt.arg); got != tt.want {
+				t.Errorf("sumvar() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCalcVar(t *testing.T) {
+	type args struct {
+		real  float64
+		ideal float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		{
+			name: "when real is 200 and ideal is 100",
+			args: args{real: 200.0, ideal: 100.0},
+			want: 100.0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := calcvar(tt.args.real, tt.args.ideal); got != tt.want {
+				t.Errorf("calcvar() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
