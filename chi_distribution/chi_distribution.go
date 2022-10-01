e97d55e8d3ce22b88117ec6938b3ec5f0d1e54ef
Taro Morita <morita0079@gmail.com>
2022-10-01 10:54:35 +0900

    x配列生成メソッド

--- chi_distribution/chi_distribution.go
+++ chi_distribution/chi_distribution.go
@@ -5,3 +5,15 @@ import "math"
 func ChiDist(dof float64, x float64) float64 {
 	return 1 / (math.Pow(2.0, dof/2.0) * math.Gamma(dof/2.0)) * math.Pow(x, dof/2.0-1.0) * math.Pow(math.E, -x/2.0)
 }
+
+func xGen(start float64,end float64, num int) []float64{
+	result := make([]float64,num)
+	dx := (end-start)/float64(num-1)
+	for i:=1;i<num;i++{
+		result[i] = result[i-1]+dx
+	}
+	result[0] = start
+	result[num-1] = end
+
+	return result
+}
